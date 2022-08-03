06ef57d33e39d7d3dd265436fa62aea2c83cb239
SamLawson1110 <sammaker1110@gmail.com>
2022-08-03 14:06:58 +0000

    first commit

---
+++ passwordvalidation/src/App.js
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import Validation from './components/Validation';
+
+function App() {
+  return (
+    <div className="App">
+      <Validation></Validation>
+    </div>
+  );
+}
+
+export default App;
---
+++ passwordvalidation/src/components/Validation.js
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+import validator from 'validator'
+ 
+const Validation = () => {
+ 
+  const [errorMessage, setErrorMessage] = useState('')
+ 
+  const validate = (value) => {
+ 
+    if (validator.isStrongPassword(value, {
+      minLength: 10, minLowercase: 1,
+      minUppercase: 1, minNumbers: 1, minSymbols: 1
+    })) {
+      setErrorMessage('')
+    } else {
+      setErrorMessage('Password needs to contain at least 1 uppercase letter, lowercase letter, number, special character, and at least 10 characters total')
+    }
+  }
+ 
+  return (
+    <div style={{
+      marginLeft: '200px',
+    }}>
+      <pre>
+        <span>Enter Password: </span><input type="text"
+          onChange={(e) => validate(e.target.value)}></input> <br />
+        {errorMessage === '' ? null :
+        <span style={{
+          fontWeight: 'bold',
+          color: 'red',
+        }}>
+            {errorMessage}
+        </span>
+        }
+      </pre>
+    </div>
+  );
+}
+ 
+export default Validation
